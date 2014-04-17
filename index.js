ee90ee0ecb71debac783bdb7cf94196bbffe7e2f
KoryNunn <knunn187@gmail.com>
2014-04-17 15:19:59 +1000

    Added deep picking, resolves #1

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var matchProp = /^([^.]*?)\.(.*?)$/,
+    revive = require('statham/revive');
+
 function cherrypick(object, exclude, properties){
     if(typeof exclude !== 'boolean'){
         properties = exclude;
@@ -6,11 +9,26 @@ function cherrypick(object, exclude, properties){
 
     typeof properties === 'string' && (properties = properties.split(' '));
 
-    var result = {};
+    var result;
 
-    for(var key in object){
-        if(~properties.indexOf(key) ? !exclude : exclude){
-            result[key] = object[key];
+    if(exclude){
+        result = revive(object);
+    }else{
+        result = {};
+    }
+
+    for(var i = 0; i < properties.length; i++){
+        var property = properties[i],
+            deep = property.match(matchProp);
+
+        if(deep){
+            result[deep[1]] = cherrypick(object[deep[1]], exclude, deep[2]);
+        }else{
+            if(exclude){
+                delete result[property];
+            }else{
+                result[property] = object[property];
+            }
         }
     }
 
--- test/index.js
+++ test/index.js
@@ -16,3 +16,19 @@ test('exclude', function(t){
 
     t.deepEqual(cherrypick(obj, true, 'a c'), {b:2});
 });
+
+test('deep include', function(t){
+    t.plan(1);
+
+    var obj = {a:1,b:2,c:3,d:{e:4}};
+
+    t.deepEqual(cherrypick(obj, 'a c d.e'), {a:1, c:3, d:{e:4}});
+});
+
+test('deep exclude', function(t){
+    t.plan(1);
+
+    var obj = {a:1,b:2,c:3,d:{e:4}};
+
+    t.deepEqual(cherrypick(obj, true, 'b d.e'), {a:1, c:3, d:{}});
+});
